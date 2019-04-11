54057c69e5e57868ca9e98b9f94018f9202541d1
dandan <kohonen_rnn@hotmail.co.jp>
2019-04-12 00:01:54 +0900

    fix main program

--- k4/RamenTimer.py
+++ k4/RamenTimer.py
@@ -1,5 +1,3 @@
-#coding: utf-8
-
 import wiringpi as pi
 import time
 import sys
@@ -8,25 +6,24 @@ LED_PIN = 23
 SW_PIN = 24
 BUZZER_PIN = 18
 
+if __name__ == '__main__':
 
-pi.wiringPiSetupGpio()
-pi.pinMode(LED_PIN, pi.OUTPUT)
-pi.pinMode(SW_PIN, pi.INPUT)
-pi.pullUpDnControl(SW_PIN, pi.PUD_UP)
-pi.pinMode(BUZZER_PIN, pi.OUTPUT)
+    pi.wiringPiSetupGpio()
+    pi.pinMode(LED_PIN, pi.OUTPUT)
+    pi.pinMode(SW_PIN, pi.INPUT)
+    pi.pullUpDnControl(SW_PIN, pi.PUD_UP)
+    pi.pinMode(BUZZER_PIN, pi.OUTPUT)
 
-
-try:
-  while True:
-    if (pi.digitalRead(SW_PIN) == pi.LOW):
-      pi.digitalWrite(LED_PIN, pi.HIGH)
-      pi.digitalWrite(BUZZER_PIN, pi.HIGH)
+    try:
+        while True:
+            if (pi.digitalRead(SW_PIN) == pi.LOW):
+                pi.digitalWrite(LED_PIN, pi.HIGH)
+                pi.digitalWrite(BUZZER_PIN, pi.HIGH)
     else:
-      pi.digitalWrite(LED_PIN, pi.LOW)
-      pi.digitalWrite(BUZZER_PIN, pi.LOW)
+        pi.digitalWrite(LED_PIN, pi.LOW)
+        pi.digitalWrite(BUZZER_PIN, pi.LOW)
     time.sleep(0.1)
 
-except KeyboardInterrupt:
-  pi.digitalWrite(BUZZER_PIN, pi.LOW)
-  sys.exit(0)
-
+    except KeyboardInterrupt:
+        pi.digitalWrite(BUZZER_PIN, pi.LOW)
+        sys.exit(0)
