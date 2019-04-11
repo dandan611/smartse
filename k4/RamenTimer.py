abb9a243da1be057151beadc0fdfa4b3cc4ff243
dandan <kohonen_rnn@hotmail.co.jp>
2019-04-12 01:24:21 +0900

    add function

--- k4/RamenTimer.py
+++ k4/RamenTimer.py
@@ -6,6 +6,10 @@ LED_PIN = 23
 SW_PIN = 24
 BUZZER_PIN = 18
 
+STANDBY = 1
+COUNTUP = 2
+NOTICE = 3
+
 if __name__ == '__main__':
 
     pi.wiringPiSetupGpio()
@@ -14,16 +18,51 @@ if __name__ == '__main__':
     pi.pullUpDnControl(SW_PIN, pi.PUD_UP)
     pi.pinMode(BUZZER_PIN, pi.OUTPUT)
 
+    time = 0
+    state = 1
+
     try:
         while True:
-            if (pi.digitalRead(SW_PIN) == pi.LOW):
-                pi.digitalWrite(LED_PIN, pi.HIGH)
-                pi.digitalWrite(BUZZER_PIN, pi.HIGH)
+
+            if (state == STANDBY):
+                print("STANDBY")
+                if (pi.digitalRead(SW_PIN) == pi.LOW):
+                    state = 2
+
+                else:
+                    pass
+
+                time.sleep(0.1)
+
+            elif (state == COUNTUP):
+                print('{}秒経過\n'.format(time))
+                if (time >= 180):
+                    state = 3
+                    pi.digitalWrite(LED_PIN, pi.HIGH)
+                    pi.digitalWrite(BUZZER_PIN, pi.HIGH)
+
+                time.sleep(1.0)
+                time += 1
+
+            elif (state == NOTICE):
+                print("180秒経過しました")
+                if (pi.digitalRead(SW_PIN) == pi.LOW):
+                    state = 1
+                    pi.digitalWrite(BUZZER_PIN, pi.LOW)
+                    pi.digitalWrite(LED_PIN, pi.LOW)
+
+                else:
+                    if((pi.digitalRead(LED_PIN) == pi.LOW):
+                        pi.digitalWrite(LED_PIN, pi.HIGH)
+                    else:
+                        pi.digitalWrite(LED_PIN, pi.LOW)
+
+                time.sleep(0.1)
+
             else:
-                pi.digitalWrite(LED_PIN, pi.LOW)
-                pi.digitalWrite(BUZZER_PIN, pi.LOW)
-            time.sleep(0.1)
+                pass
 
     except KeyboardInterrupt:
+        pi.digitalWrite(LED_PIN, pi.LOW)
         pi.digitalWrite(BUZZER_PIN, pi.LOW)
         sys.exit(0)
